89449744e423d0c888f5e4e76dcc779506b76a9f
sorayade <sorayadelreymartinez@gmail.com>
2022-06-16 21:31:27 +0200

    actualización 1era entrega

--- js/main.js
+++ js/main.js
@@ -86,7 +86,6 @@ producto2 = prompt(
 if(producto2 == "SALIR") alert("Gracias, que tengas un buen día");
 
 }
-*/
 
 
 //arrays - productos con mismo precio + IVA - imprimir en consola
@@ -103,3 +102,61 @@ for (const producto of productos){
     console.log("El id del producto es " + producto.id);
     console.log("El producto " + producto.producto + " tiene un precio de " + resultado + "€ IVA incl.");
 }
+*/
+
+
+//Primera entrega proyecto final
+
+function saludar(){
+alert("Bienvenid@ a nuestra tienda online de Papeleria Creativa");
+}
+
+const IVA = 0.21;
+let precio=20;
+let precio2=40;
+let resultado = precio * IVA + precio ;
+let resultado2 = precio2 * IVA + precio2 ;
+
+
+
+const productoColor = [{id: 1, color: "azul"}, 
+{id: 2, color: "blanco"},
+{id: 3, color: "rojo"},
+{id: 4, color: "amarillo"}, 
+{id: 5, color: "marrón"}];
+
+saludar();
+
+let producto2 = prompt(
+    "Ingresa una opción. Para salir ingresa SALIR \n 1- Cuaderno \n 2- Bases de madera \n 3- Láminas \n 4- Libros craft \n 5- Acuarelas"
+);
+
+
+while(producto2 != "SALIR"){
+switch(producto2){
+    case "1":
+    alert("Has seleccionado el producto Cuaderno y su precio es " + resultado + "€" + "en color " + productoColor[1].color);
+    break;
+    case "2":
+    alert("Has selecionado el producto Bases de madera y su precio es " + resultado + "€"+ " en color " + productoColor[4].color + " y " + resultado2 + "€" + " en color " + productoColor[3].color);
+    break
+    case "3":
+    alert("Has seleccionado el producto Láminas y su precio es " + resultado + "€" + "en color " + productoColor[2].color);
+    break
+    case "4":
+    alert("Has seleccionado el producto Libros Craft y su precio es " + resultado + "€" + "en color " + productoColor[5].color);
+    break
+    case "5":
+    alert("Has seleccionado el producto Acuarelas y su precio es " + resultado + "€" + color.id+ "en color " + productoColor[3].color);
+    break
+    default:
+        alert("opción no válida");
+}
+
+producto2 = prompt(
+    "ingresa una opción. Para salir ingresa SALIR \n 1- Cuaderno \n 2- Bases de madera \n 3- Láminas \n 4- Libros craft \n 5- Acuarelas"
+);
+if(producto2 == "SALIR") alert("Gracias, que tengas un buen día. A continuación, te mostramos nuestra oferta de cartulinas por consola");
+
+}
+
