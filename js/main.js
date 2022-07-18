8453827d652cf332a40a0a72d4c3da1ebc8ccf2e
sorayade <sorayadelreymartinez@gmail.com>
2022-07-18 16:00:23 +0200

    fetch

--- js/main.js
+++ js/main.js
@@ -277,7 +277,7 @@ class producto {
   }
 
   console.log(arrayProductos);
-*/
+
 
 //segunda entrega login - Registro con JSON y localStorage
 
@@ -371,3 +371,54 @@ document.getElementById('sweetalert').addEventListener('click', function(){
   
     });
 
+*/
+
+//Envío formulario con Fetch 
+
+
+const d = document;
+
+function contactForm(){
+    const $form = d.querySelector(".contact-form"),
+    $inputs = d.querySelectorAll(".contact-form [required");
+}
+
+//console.log($inputs)
+
+$inputs.forEach((input)=> {
+
+    const $span = d.createElement("span");
+    $span.id = input.name;
+    $span.textContent = input.title;
+    $span.classList.add("contact-form-error", "none");
+    input.insertAdjacentElement("afterend", $span);
+    
+});
+
+
+d.addEventListener("keyup", (e) =>{
+    if(e.target.matches(".contact-form [required]")){
+        let $input = e.target, 
+        pattern = $input.pattern || $input.dataset.pattern;
+    }
+})
+
+d.addEventListener("submit", (e) =>{
+    alert("Enviando Formulario");
+
+
+const $loader = d.querySelector(".contact-form-loader"),
+$response = d.querySelector(".contact-form-response");
+
+$loader.classList.remove("none");
+
+setTimeout(() =>{
+    $loader.classList.add("none");
+    $response.classList.remove("none");
+    $form.reset();
+
+    setTimeout(() => $response.classList.add("none"), 3000);
+}, 3000);
+});
+
+d.addEventListener("DOMContentLoaded", contactForm)
